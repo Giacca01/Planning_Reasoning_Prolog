7d90d1a04ae41c2f2a0100ccacaa81551848d415
Giacca01 <fgiacardi01@gmail.com>
2024-07-08 22:51:16 +0200

    Version 0.5: final mazes testing.

--- Prolog/Labirinti/80x80/labirinto_0.pl
+++ Prolog/Labirinti/80x80/labirinto_0.pl
@@ -1,24 +1,6 @@
 num_col(80).
 num_righe(80).
-iniziale(pos(6, 29)).
-finale(pos(78, 4)).
-finale(pos(78, 57)).
-occupata(pos(76, 3)).
-occupata(pos(76, 4)).
-occupata(pos(76, 5)).
-occupata(pos(76, 6)).
-occupata(pos(77, 6)).
-occupata(pos(78, 6)).
-occupata(pos(27, 12)).
-occupata(pos(28, 13)).
-occupata(pos(29, 14)).
-occupata(pos(30, 15)).
-occupata(pos(31, 16)).
-occupata(pos(32, 17)).
-occupata(pos(33, 18)).
-occupata(pos(40, 32)).
-occupata(pos(41, 32)).
-occupata(pos(36, 32)).
-occupata(pos(37, 32)).
-occupata(pos(38, 32)).
-occupata(pos(39, 32)).
+iniziale(pos(1, 1)).
+finale(pos(80, 48)).
+finale(pos(52, 77)).
+occupata(pos(4, 78)).
